bdd28c0f93bd7c038cc6ce35e109c028df1f01e5
ryuya33 <ryuya33@users.noreply.github.com>
2025-06-26 12:47:04 +0900

    🐛 Improve favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,7 +48,7 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/favicons/favicon-96x96.png", sizes: "96x96", type: "image/png" },
-      { url: "/favicons/favicon.svg", type: "svg+xml" },
+      { url: "/favicons/favicon.svg", type: "image/svg+xml" },
     ],
     apple: "/favicons/apple-touch-icon.png",
     shortcut: "/favicon.ico",
